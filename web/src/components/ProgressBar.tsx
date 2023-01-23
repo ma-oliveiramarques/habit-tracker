ded9a02630cc406f8e3118143765ec3113c123e3
Mariana Marques <ma_oliveiramarques@hotmail.com>
2023-01-23 02:06:13 +0000

    frontend finished

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -5,13 +5,15 @@ interface ProgressBarProps {
 export function ProgressBar(props: ProgressBarProps) {
   return (
     <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
-    <div
-      role="progressbar"
-      aria-label="Progresso de hábitos completados nesse dia"
-      aria-valuenow={props.progress}
-      className='h-3 rounded-xl bg-violet-600'
-      style={{ width: `${props.progress}%`}}
-     />
-  </div>
+      <div 
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600 transition-all"
+        style={{
+          width: `${props.progress}%`
+        }}
+      />
+    </div>
   )
 }
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,4 +1,3 @@
-
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
@@ -29,13 +28,13 @@ type Summary = {
 
 export function SummaryTable() {
 
-  const [summary, setSummary] = useState<Summary>([]) 
+  const [summary, setSummary] = useState<Summary>([])
 
-  useEffect(() => {
-    api.get('/summary').then(response => {
+  useEffect(() =>{
+    api.get('summary').then(response => {
       setSummary(response.data)
     })
-  }, [])
+  },[])
 
   return (
     <div className="w-full flex">
@@ -53,18 +52,18 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
-          
+        {summary.length &&summaryDates.map((date) => {
+
           const dayInSummary = summary.find(day => {
             return dayjs(date).isSame(day.date, 'day')
           })
 
           return (
             <HabitDay 
-              key={date.toString()} 
+              key={date.toString()}
               date={date}
               amount={dayInSummary?.amount} 
-              completed={dayInSummary?.completed} 
+              defaultCompleted={dayInSummary?.completed} 
             />
           )
         })}
