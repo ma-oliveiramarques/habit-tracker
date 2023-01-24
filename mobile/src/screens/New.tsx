fc761af8d1594964795d4e4f35891a24df2a1ea8
Mariana Marques <ma_oliveiramarques@hotmail.com>
2023-01-24 16:46:09 +0000

    update screen

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
-import { ScrollView, View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
-import { Feather } from "@expo/vector-icons"
-import colors from "tailwindcss/colors"
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 import { api } from "../lib/axios";
 
-const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira','Sexta-feira', 'Sábado'];
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
-  const [title, setTitle] = useState('') 
-  const [weekDays, setWeekDays] = useState<number[]>([]) 
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState('');
 
   function handleToggleWeekDay(weekDayIndex: number) {
-    if(weekDays.includes(weekDayIndex)) {
+    if (weekDays.includes(weekDayIndex)) {
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex])
     }
   }
 
-  async function handleCreateNewHabit(){
-    try{
+  async function handleCreateNewHabit() {
+    try {
       if (!title.trim() || weekDays.length === 0) {
-        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade')
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.')
       }
 
       await api.post('/habits', { title, weekDays })
@@ -32,7 +32,7 @@ export function New() {
       setTitle('');
       setWeekDays([]);
 
-      Alert.alert('Novo hábito', 'Hábito criado com sucesso!')
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
     } catch (error) {
       console.log(error)
       Alert.alert('Ops', 'Não foi possível criar o novo hábito')
@@ -45,20 +45,19 @@ export function New() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
-        
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
-          Criar Hábito
+          Criar hábito
         </Text>
 
         <Text className="mt-6 text-white font-semibold text-base">
-          Qual o seu comprometimento?
+          Qual seu comprometimento?
         </Text>
 
         <TextInput 
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
-          placeholder="ex: Exercícios, dormir bem, etc..."
+          placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
           onChangeText={setTitle}
           value={title}
@@ -70,7 +69,7 @@ export function New() {
 
         {
           availableWeekDays.map((weekDay, index) => (
-            <Checkbox 
+            <Checkbox
               key={weekDay}
               title={weekDay}
               checked={weekDays.includes(index)}
@@ -79,21 +78,20 @@ export function New() {
           ))
         }
 
-        <TouchableOpacity 
+        <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
           onPress={handleCreateNewHabit}
         >
           <Feather 
-            name="check"
-            size={20}
-            color={colors.white}
+           name="check"
+           size={20}
+           color={colors.white}
           />
 
           <Text className="font-semibold text-base text-white ml-2">
             Confirmar
           </Text>
-          
         </TouchableOpacity>
       </ScrollView>
     </View>
